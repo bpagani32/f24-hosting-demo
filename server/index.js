a75da666cf82a33680b307a2449faa5dfefc0d0a
Brian Pagani <brianpagani@yahoo.com>
2022-06-29 12:17:43 -0600

    please work

--- server/index.js
+++ server/index.js
@@ -8,6 +8,9 @@ const app = express()
 app.get('/', (req,res) =>{
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+app.get('/css',(req,res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
 const port = process.env.PORT || 4005
 
 
