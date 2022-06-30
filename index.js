4ef1fd55b7e01bf65812ddb88fc1d8af8d1fefe0
Brian Pagani <brianpagani@yahoo.com>
2022-06-30 16:34:27 -0600

    bp32

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ const message = () => {
 }
 
 bigButton.addEventListener('click', message)
+
+
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'e07644adba114a18935ae6b3dd14bf3c',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+
+try {blah()} 
+catch{bad} rollbar.critical(err)
--- server/index.js
+++ server/index.js
@@ -18,5 +18,30 @@ const port = process.env.PORT || 4005
 
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server jammin on port ${port}!`)
 })
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: 'e07644adba114a18935ae6b3dd14bf3c',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+try {blah()} 
+catch{bad} rollbar.critical(err)
+const students = ['Jimmy', 'Timothy', 'Jimothy']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+    rollbar.info('Students List Requested')
+    rollbar.error('stuff is going wrong')
+    rollbar.critical('everything is on fire')
+    rollbar.warning('jk')
+    res.status(200).send(students)
+
+
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
